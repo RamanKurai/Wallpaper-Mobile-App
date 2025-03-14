40e94c3953779d4f18108e104d71185f3d781aed
Raman Kurai <ramankurai27@gmail.com>
2025-03-14 22:12:29 +0530

    feat :  material top bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,7 +3,9 @@ import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue',
+      headerShown : false
+     }}>
       <Tabs.Screen
         name="index"
         options={{
@@ -11,18 +13,18 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
         }}
       />
+       <Tabs.Screen
+        name="accounts"
+        options={{
+          title: 'Account',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} />,
+        }}
+      />
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
           tabBarIcon: ({ color }) => <FontAwesome size={28} name="globe" color={color} />,
-        }}
-      />
-       <Tabs.Screen
-        name="accounts"
-        options={{
-          title: 'Accounts',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} />,
         }}
       />
     </Tabs>
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,29 @@
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { Text, View } from 'react-native';
+
+const Tab = createMaterialTopTabNavigator();
+
+export default function ForYou() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
+
+function HomeScreen() {
+  return   <View>
+      <Text>
+        HomeScreen Only
+      </Text>
+    </View>
+}
+
+function ProfileScreen() {
+   return <View>
+    <Text>
+      ProfileScreen Only
+    </Text>
+  </View>
+}
