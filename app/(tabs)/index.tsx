666d3acb0c5d6a896a83989e09e4c93c66f38252
Raman Kurai <ramankurai27@gmail.com>
2025-03-16 17:13:28 +0530

    update : ImageCardin explore screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,60 @@
 // https://ideogram.ai/assets/progressive-image/balanced/response/UL_6QboVRnOKm2o24N28_g
 // https://ideogram.ai/assets/image/lossless/response/TtdkwgiJSQmyJra5XeN0ww
 // https://ideogram.ai/assets/progressive-image/balanced/response/mbsPKwZATHKECmlcB3iobA
+// https://ideogram.ai/assets/progressive-image/balanced/response/OhFc9ol_QEqloFNTnnWMKw
+import { ImageCard } from "@/components/ImageCard";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { Image, SafeAreaView, Text, View } from "react-native";
+import { ThemedView } from "@/components/ThemedView";
+import { useWallpapers } from "@/hooks/useWallpapers";
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { FlatList, GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function Explore() {
+  const wallpapers = useWallpapers();
   return (
+    <GestureHandlerRootView>
     <SafeAreaView 
     style={{flex: 1}}>
       <ParallaxScrollView
         headerBackgroundColor={{ dark: "black ", light: "white" }}
         headerImage={
-          <Image
-            style={{ 
-            flex : 1,
-            }}
-            source={{
-              uri: "https://ideogram.ai/assets/image/lossless/response/TtdkwgiJSQmyJra5XeN0ww",
-            }}
-          />
-        }
-      >
-    <View style={{ minHeight: 1000 , padding: 20 }}>
-    <Text style={{ color: "white", fontSize: 24 }}>Explore Page</Text>
-  </View>
+          <Image style={{flex : 1,}} source={{uri: wallpapers[0]?.url ?? "" }} />}>
+            <ThemedView style={styles.container}>
+              <ThemedView style={styles.innerContainer}>
+               <FlatList
+                 data={wallpapers.filter((_, index) => index % 2 === 0)}
+                 renderItem={({item}) => <View style={styles.imageContainer}>
+                  <ImageCard  wallpaper={item}/>
+                 </View>}
+                 keyExtractor={item => item.name}
+               />
+              </ThemedView>
+              <ThemedView style={styles.innerContainer}>
+              <FlatList
+                 data={wallpapers.filter((_, index)=> index % 2 === 1)}
+                 renderItem={({item}) => <View style={styles.imageContainer}>
+                  <ImageCard  wallpaper={item}/>
+                 </View>}
+                 keyExtractor={item => item.name}
+               />
+              </ThemedView>
+            </ThemedView>
       </ParallaxScrollView>
     </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+      flexDirection: "row",
+      flex: 1
+  },
+  innerContainer: {
+      flex: 1,
+      padding: 10
+  },
+  imageContainer: {
+      paddingVertical: 10
+  }
+})
